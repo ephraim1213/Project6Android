e513d39a6b5ea5047f3f6025d3426b22a3d7b8c1
Ephraim <ephraim1213@yahoo.com>
2025-05-06 21:45:45 -0600

    onto page 7

--- app/src/main/java/com/example/project6android/MainActivity.kt
+++ app/src/main/java/com/example/project6android/MainActivity.kt
@@ -19,6 +19,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.ui.res.painterResource
 import androidx.compose.material3.Button
 import androidx.compose.ui.res.stringResource
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.ui.unit.dp
 
 
 
@@ -56,6 +59,7 @@ fun DiceWithButtonAndImage(modifier : Modifier = Modifier) {
             painter = painterResource(R.drawable.dice_1),
             contentDescription = "1"
         )
+        Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = { /*TODO*/}) {
             Text(stringResource(R.string.roll))
         }
